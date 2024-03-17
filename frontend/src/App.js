0f4e86607f041184d79b61dc5135a70e872b6878
Aman <iec2022063@iiita.ac.in>
2024-03-17 23:32:59 +0530

    created header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
+import LandingPage from "./Screens/LandingPage/LandingPage";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
-const App = () => <h1 className="App">Hello World</h1>;
+const App = () => (
+  <>
+    <Header />
+    <main>
+      <LandingPage />
+    </main>
+    <Footer />
+  </>
+);
 
 export default App;

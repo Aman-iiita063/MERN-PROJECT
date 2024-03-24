039ec814340e86ee74f4fd092bf0db31a25aa00b
Aman <iec2022063@iiita.ac.in>
2024-03-25 03:09:37 +0530

    connected and backend and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
 import LandingPage from "./Screens/LandingPage/LandingPage";
+import MyNotes from "./Screens/MyNotes/MyNotes";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => (
-  <>
+  <BrowserRouter>
     <Header />
     <main>
-      <LandingPage />
+      <Routes>
+        <Route path="/" Component={LandingPage} exact />
+        <Route path="/mynotes" Component={() => <MyNotes />} />
+      </Routes>
     </main>
     <Footer />
-  </>
+  </BrowserRouter>
 );
 
 export default App;
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -6,12 +6,15 @@ import {
   NavDropdown,
   Navbar,
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Navbar bg="primary" expand="lg" variant="dark">
       <Container>
-        <Navbar.Brand href="/">Note Zipper</Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/">Note Zipper</Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav className="m-auto">
@@ -28,7 +31,9 @@ const Header = () => {
             style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <Nav.Link href="#action1">My Notes</Nav.Link>
+            <Nav.Link href="/mynotes">
+              <Link to="/mynotes">My Notes</Link>
+            </Nav.Link>
             <NavDropdown title="Aman Kumar" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">My Profile</NavDropdown.Item>
 
